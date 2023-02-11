f3f820d482d715427419c26d6e2a976da44071ca
Ganda1ff <ivanrotov2020@gmail.com>
2023-02-11 16:00:02 +0400

    first

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+<header class="el_header">
+    <div class="el_logo"><a href=""></a></div>
+            <a href="" class="el_a">
+                <button class="el_header_button">КАТАЛОГ ИГР</button>
+            </a>
+            <input class="el_header_input" type="text" placeholder="Введите название игры">
+            <a href="" class="el_a">
+                <button class="el_header_button">ВОЙТИ</button>
+            </a>
+            <a class="el_shopping_cart" href="#"></a>
+          
+</header>
+<main>
+<div class="slideshow-container">
+
+
+    <div class="mySlides fade">
+        <div class="appear">
+            <h1 class="el_h1_appear">НАШИ ЛУЧШИЕ ПРЕДЛОЖЕНИЯ</h1>
+            <a href="#"><button class="el_button_appear">В КОРЗИНУ</button></a>
+        </div>
+        <div class="price">
+            <h3 class="price_number">899₽</h3>
+            <h3 class="discount_amount">50%</h3>
+        </div>
+        <!-- <a href="#"><img class="el_image_blured" src="images/rdr2.jpg" style="width:100%"></a> -->
+        
+        
+    </div>
+
+    <div class="mySlides fade">
+    <div class="appear">
+            <h1 class="el_h1_appear">НАШЕ ЛУЧШЕЕ ПРЕДЛОЖЕНИЕ</h1>
+            <a href="#"><button class="el_button_appear">КУПИТЬ</button></a>
+        </div>
+        <img class="el_image_blured" src="images/cyberpunk.jpg" style="width:100%">
+        
+    </div>
+
+    <div class="mySlides fade">
+    <div class="appear">
+            <h1 class="el_h1_appear">НАШЕ ЛУЧШЕЕ ПРЕДЛОЖЕНИЕ</h1>
+            <a href="#"><button class="el_button_appear">КУПИТЬ</button></a>
+        </div>
+        <img class="el_image_blured" src="images/the_witcher3.jpg" style="width:100%">
+        >
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+
+<br>
+
+<container class="items_for_sale">
+    <h1 class="items_for_sale_heading">ВСЕ ИГРЫ</h1>
+    <div class="shopping_list">
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+            <a href="#"><h3 class="shop_item_name">Red Dead Redemption 2</h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price">899₽</h3>
+        
+                <button class="add_to_cart">В КОРЗИНУ</button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="shop_item">
+        <img src="images/rdr2.jpg" alt="">
+        <div class="shop_item_description">
+        <a href="#"><h3 class="shop_item_name"></h3></a>
+            <div class="price_flex">
+
+                <h3 class="shop_item_price"></h3>
+        
+                <button class="add_to_cart"></button>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="switch_page">
+    <ul class="switch_page_ul">
+        
+        <a href="#"><li class="switch_page_li">1</li></a>
+        <a href="#"><li class="switch_page_li">2</li></a>
+        <a href="#"><li class="switch_page_li">3</li></a>
+        <a href="#"><li class="switch_page_li">4</li></a>
+        <a href="#"><li class="switch_page_li">5</li></a>
+    </ul>
+
+    <input class="checked_input" type="radio" id="button">
+<label class="checked_input_label" for="button">Click</label>
+
+<input class="checked_input" type="radio" id="button">
+<label class="checked_input_label" for="button">Click</label>
+
+</div>
+</container>
+
+
+</main>
+<script src="slide_gallery.js"></script>
+
+</body>
+</html>
---
+++ slide_gallery.js
@@ -0,0 +1,49 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+}
+
+// let els_imageBlock = document.querySelectorAll(".mySlides");
+//     for (let el_imageBlock of els_imageBlock) {
+//         el_imageBlock.addEventListener('mouseenter', ()=>{
+//             let els_appearingBlock = document.querySelectorAll(".appear");
+//             for (let el_appearingBlock of els_appearingBlock) {
+//                    el_appearingBlock.style.display = "block"; 
+//         }
+//         el_imageBlock.addEventListener('mouseleave', ()=>{
+//             for (let el_appearingBlock of els_appearingBlock) {
+//                 el_appearingBlock.style.display = "none";
+
+//             }
+            
+            
+//         });
+       
+//         });
+//       }
+
+let el_btn = document.querySelector(".switch_page_li");
+el_btn.addEventListener("click", function() {
+  this.classList.add("active");
+});
+
